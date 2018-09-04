37480afd0e20feb44fef25da36a443ad8d3099e1
Nayanda Haberty <nayanda.catch@gmail.com>
2018-09-04 13:54:24 +0700

    create a test (on progress) <nayanda.haberty>

--- NativefierTest/NativefierTest.cs
+++ NativefierTest/NativefierTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Nativefier;
 using Nativefier.Model;
@@ -9,7 +10,7 @@ namespace NativefierTest
     [TestClass]
     public class NativefierTest : IDiskCacheDelegate<TestModel>, IMemoryCacheDelegate<TestModel>
     {
-        public static Nativefier<TestModel> Nativefier { get; set; }
+        public static INativefier<TestModel> Nativefier { get; set; }
         
         [TestInitialize]
         public void Start()
@@ -23,6 +24,8 @@ namespace NativefierTest
         [TestMethod]
         public void TestStoreOneItem()
         {
+            var model = GenerateTestModel();
+            Nativefier.Put(model, "one");
         }
 
         [TestMethod]
@@ -33,6 +36,32 @@ namespace NativefierTest
         [TestMethod]
         public void TestStoreFiveItem()
         {
+        }
+
+        private TestModel GenerateTestModel()
+        {
+            return new TestModel(
+                RandomString(50), RandomInt(0, 50), RandomDouble(0, 50), 
+                new SubTestModel(
+                    RandomString(50), RandomInt(50, 100), RandomDouble(50, 100)));
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static int RandomInt(int bottom, int top)
+        {
+            return random.Next(bottom, top);
+        }
+
+        public static double RandomDouble(int bottom, int top)
+        {
+            return RandomInt(bottom, top);
         }
 
         // DELEGATE
